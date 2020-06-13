46fdf3ee3f832d4d272143bd9544033936f883ec
tranducanhdragon <35365150+tranducanhdragon@users.noreply.github.com>
2020-06-13 14:57:50 +0700

    Add files via upload

--- QuanLiDuAn/TriggerInserted.sql
+++ QuanLiDuAn/TriggerInserted.sql
@@ -1,4 +1,4 @@
-﻿select * from SUDUNGDICHVU
+﻿--select * from SUDUNGDICHVU
 create proc SuDungDichVu_Insert(
     @TenDangNhap nvarchar(200),
 	@MaNV nchar(100),
@@ -22,3 +22,26 @@ end
 drop trigger DichVu_Inserted
 exec SuDungDichVu_Insert 'ducanh', 'NV01', '5', '0', 1
 select * from DICHVU
+--Tạo trigger insert Dịch vụ nếu trùng tên dịch vụ thì ko đc insert
+alter trigger DichVu_Insert on DichVu for insert
+as
+begin
+	if(select count(DichVu.TenDV) from DICHVU right join inserted on DICHVU.TenDV = inserted.TenDV) > 1
+	begin
+	    print(N'Tên dịch vụ đã tồn tại')
+		rollback tran
+	end
+end
+insert into DichVu values('8','Nem chua rán','Đồ ăn',8000, 10)
+select * from DichVu
+--tạo trigger ko cho phép xóa dịch vụ có tên là Giờ chơi
+alter trigger DichVu_Delete on DichVu for delete
+as
+begin
+    if exists(select * from deleted where TenDV = N'Giờ chơi')
+	begin
+	    print(N'Không được xóa dịch vụ này!')
+		rollback tran
+	end
+end
+delete from DICHVU where MaDV = '1'
